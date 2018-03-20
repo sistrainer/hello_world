6e1360cd4e31e2c2af133680033e810afa7cb324
surendra <sis.java.training@gmail.com>
2018-03-20 12:52:11 +0530

    commited hello word base v1

---
+++ HappyProgrammng1/src/Greet.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+
+/*
+@Author:Surendra Kumar Sao
+	>>Software Architect and Corporate Trainer
+	>>+9 year exp in (mumbai,pune,hyd,bangaluru)
+	>>Java Certified SCJP & SCWCD with 98%
+	Trained more than 5k students and employees.
+    MCA from (NIT)National Institute of Technology Raipur(C.G.)
+	https://www.urbanpro.com/raipur/surendra-kumar-sao/reviews/7223178
+	https://www.urbanpro.com/raipur/surendra-kumar-sao/1334109?_tp=
+ 
+   CurrentYear 2018 
+ */
+
+public class Greet {
+	public static void main(String[] args) {
+		String sisMsg ="Success is sure if you want :)";
+		JOptionPane.showMessageDialog(null, sisMsg);
+
+	}
+
+}
